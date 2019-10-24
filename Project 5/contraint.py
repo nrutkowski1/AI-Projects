4c8a904944dbd907dd90724b1d7b168afc9c0660
Nathaniel Rutkowski <41529200+nrutkowski1@users.noreply.github.com>
2019-10-24 11:56:25 -0400

    Update contraint.py

--- Project 5/contraint.py
+++ Project 5/contraint.py
@@ -1,19 +1,4 @@
-# Project 5 - CSP
-# Theresa Inzerillo & Preston Mueller
-# CS4341 Introduction to Artificial Intelligence
-#
-# Constraint object class
 
-# class Constraint():
-#
-# 	def __init__(self):
-# 		self.bag_min = 0
-# 		self.bag_max = 0
-# 		self.binaryequals = []
-# 		self.binarynotequals = []
-# 		self.un_incl = {}
-# 		self.un_excl = {}
-# 		self.bin_sim = {}
 
 
 class Constraint():
